a3c880a6e6334baa700e04bf949ace56c535edbd
David Nyffenegger <david.nyffenegger@namics.com>
2013-09-19 16:09:46 +0200

    Adds canvas

--- public/js/app.js
+++ public/js/app.js
@@ -34,3 +34,67 @@ socket.on('maximum reached', function(data) {
 });
 
 // socket.emit('leave', { room: room});
+
+
+window.onload = function() {
+        
+        var louisIsHere = false;
+        
+		var canvas = document.getElementById('canvas-louie');
+		paper.setup(canvas);
+		var view = paper.view;
+		
+		var tool = new paper.Tool();
+		
+        var louie = new paper.Path.Circle({
+            center: view.bounds.leftCenter,
+            radius: 70,
+            fillColor: 'red'
+        });
+        
+        var velocity = new paper.Point(10.0, 0.0);
+        var acceleration = new paper.Point(0, 0.9);
+        
+        var updateLouis = function() {
+          velocity.x += acceleration.x;
+          velocity.y += acceleration.y;
+          louie.position.x += velocity.x;
+          louie.position.y += velocity.y;
+          
+          var hitBottom = louie.bounds.bottom >= view.bounds.bottom;
+          var hitTop = louie.bounds.top <= view.bounds.top;
+          var hitRight = louie.bounds.left > view.bounds.right;
+          
+          if (hitBottom) {
+            velocity.y *= -0.8;
+            louie.position.y = view.bounds.bottom - louie.bounds.height/2.0;
+          }
+          if (hitTop) {
+            velocity.y *= -0.8;
+            louie.position.y = louie.bounds.height/2.0;    
+          }
+          
+          if (hitRight) {
+            // notify
+            socket.emit('end', {
+                room: room, 
+                x: 2, 
+                y: 7,
+                vx: 5, 
+                vy: 7
+            });
+            louie.position.x = 0;
+          }
+        };
+        
+        view.onFrame = function() {        
+          updateLouis();
+          
+          
+        };
+        
+        tool.onMouseDrag = function(event) {
+            velocity.y += event.delta.y / 10.0;
+        };
+		
+	}
