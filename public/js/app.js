aa3737c997e8102b44672ab0fc84154d12daf085
Remo Brunschwiler <remo@terrifically.org>
2013-09-20 14:54:44 +0200

    Merge branch 'master' of /Users/rbrunschwiler/Documents/github/looping-louie with conflicts.

--- public/js/app.js
+++ public/js/app.js
@@ -3,102 +3,108 @@ var socket = io.connect('http://' + document.location.hostname);
 
 window.onload = function() {
 
-    var canvas = document.getElementById('canvas-louie');
-    paper.setup(canvas);
-    var view = paper.view;
-    var tool = new paper.Tool();
-    var groundHeightScale = 1 / 6;
+    // display splash screen
+    setTimeout(function() {
+        var splash = document.body;
+        splash.style.backgroundImage = 'none';
+
+        var canvas = document.getElementById('canvas-louie');
+        paper.setup(canvas);
+        var view = paper.view;
+        var tool = new paper.Tool();
+        var groundHeight = view.bounds.height / 6;
 
 
-    var app = {
-        view: view,
-        predecessorsChicken: {},
-        chickenId: 0,
-        player: {},
+        var app = {
+            view: view,
+            predecessorsChicken: {},
+            chickenId: 0,
+            player: {},
 
-        pickedChicken: function() {
-            socket.emit('score', { room: room });
-        },
+            pickedChicken: function() {
+                socket.emit('score', { room: room });
+            },
 
-        chickenIsAboutLeaving: function(data) {
-            data.room = room;
-            data.msg = 'chickenArrives';
-            socket.emit('tellNext', data );
-        },
+            chickenIsAboutLeaving: function(data) {
+                data.room = room;
+                data.msg = 'chickenArrives';
+                socket.emit('tellNext', data );
+            },
 
-        louieIsAboutLeaving: function(data) {
-            data.room = room;
-            socket.emit('end', data);
-        }
+            louieIsAboutLeaving: function(data) {
+                data.room = room;
+                socket.emit('end', data);
+            }
 
-    };
+        };
 
-    app.louie = new Louie(app, groundHeightScale / 2);
-    app.statusBar = new StatusBar(app);
+        app.louie = new Louie(app);
+        app.statusBar = new StatusBar(app);
 
-    view.onFrame = function() {
-        app.louie.onFrame();
-        if (app.chicken)
-            app.chicken.onFrame();
-        for(var i in app.predecessorsChicken) {
-            if (app.predecessorsChicken[i].isActive())
-                app.predecessorsChicken[i].onFrame();
-            else
-                delete app.predecessorsChicken[i];
-        }
-    };
+        view.onFrame = function() {
+            app.louie.onFrame();
+            if (app.chicken)
+                app.chicken.onFrame();
+            for(var i in app.predecessorsChicken) {
+                if (app.predecessorsChicken[i].isActive())
+                    app.predecessorsChicken[i].onFrame();
+                else
+                    delete app.predecessorsChicken[i];
+            }
+        };
 
-    view.onResize = function() {
-        app.louie.resize();
-        app.chicken.resize();
-        app.statusBar.resize();
-    };
+        view.onResize = function() {
+            app.louie.resize();
+            app.chicken.resize();
+            app.statusBar.resize();
+        };
 
-    tool.onMouseDrag = function(event) {
-        app.louie.push(event.delta);
-    };
+        tool.onMouseDrag = function(event) {
+            app.louie.push(event.delta);
+        };
 
-    socket.on('welcome', function (data) {
-        app.player = data;
-        app.statusBar.ground(data.color.normal, groundHeightScale);
-        app.chicken = new Chicken(app, 'chicken_' + app.player.color.chickenColor + '-', data.lives, groundHeightScale / 2);
-    });
+        socket.on('welcome', function (data) {
+            app.player = data;
+            app.statusBar.ground(data.color.normal, groundHeight);
+            app.chicken = new Chicken(app, 'chicken_' + app.player.color.chickenColor + '-', data.lives);
+        });
 
-    socket.on('start', function (data) {
-        app.louie.appears(data);
-    });
+        socket.on('start', function (data) {
+            app.louie.appears(data);
+        });
 
-    socket.on('msgFromPredecessor', function(data) {
-        if (data.msg == 'chickenArrives') {
-            var chicken = new PredecessorChicken(app);
-            chicken.appears(data);
-            app.predecessorsChicken[app.chickenId++] = chicken;
-        }
+        socket.on('msgFromPredecessor', function(data) {
+            if (data.msg == 'chickenArrives') {
+                var chicken = new PredecessorChicken(app);
+                chicken.appears(data);
+                app.predecessorsChicken[app.chickenId++] = chicken;
+            }
 
-    });
+        });
 
-    // receive
-    socket.on('position', function (data) {
-        // update position (status bar)
-        app.statusBar.updatePosition(data);
-    });
-
-
-    socket.on('state', function (data) {
-        // update state -> redraw players etc.
-        app.statusBar.update(data);
-    });
+        // receive
+        socket.on('position', function (data) {
+            // update position (status bar)
+            app.statusBar.updatePosition(data);
+        });
 
 
-    // join room
-    socket.emit('join', { room: room});
+        socket.on('state', function (data) {
+            // update state -> redraw players etc.
+            app.statusBar.update(data);
+        });
 
-    socket.on('maximum reached', function(data) {
-        // display maximum players reached message
-        alert('only ' + data.maxConnections + ' players can play together');
-    });
 
-    // socket.emit('leave', { room: room});
+        // join room
+        socket.emit('join', { room: room});
+
+        socket.on('maximum reached', function(data) {
+            // display maximum players reached message
+            alert('only ' + data.maxConnections + ' players can play together');
+        });
+
+        // socket.emit('leave', { room: room});
+    }, 2000);
 }
 
 
